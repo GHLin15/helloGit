a6d6e61554813fc9cf996a85d77873a1c4ddb9aa
liren15 <lllxzr15@163.com>
2020-09-04 14:05:29 +0800

    添加1

---
+++ HelloGit/src/main/java/com/liren/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.liren.git;
+
+/**
+ * @author liren
+ * @create 2020-09-04 13:49
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
