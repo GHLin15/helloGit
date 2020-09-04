cd8cede1b1cb38d32cd48caed72350deb7e71c8e
liren15 <lllxzr15@163.com>
2020-09-04 14:28:25 +0800

    dev-add

--- HelloGit/src/main/java/com/liren/git/HelloGit.java
+++ HelloGit/src/main/java/com/liren/git/HelloGit.java
@@ -7,5 +7,6 @@ package com.liren.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit");
+        System.out.println("dec add");
     }
 }
